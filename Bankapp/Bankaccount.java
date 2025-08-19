610756290a286fd22ea01cca6d6215adb0be96d7
Asit666 <abhisekmohapatra.2013@gmail.com>
2025-08-19 22:24:36 +0530

    Update Bankaccount.java

--- Bankapp/Bankaccount.java
+++ Bankapp/Bankaccount.java
@@ -5,44 +5,39 @@ public class Bankaccount {
     private String pin;
     private double balance;
 
-    public Bankaccount(String accountnumber,String pin,double balance)
-    {
-       this.accountnumber = accountnumber;
-       this.pin = pin;
-       this.balance = balance;
+    public Bankaccount(String accountnumber, String pin, double balance) {
+        this.accountnumber = accountnumber;
+        this.pin = pin;
+        this.balance = balance;
     }
 
-    public double getbalance()
-    {
+    public double getbalance() {
         return balance;
     }
 
-    public String getaccountnumber()
-    {
+    public String getaccountnumber() {
         return accountnumber;
     }
 
-
-    public boolean checkpin(String pin)
-    {
+    public boolean checkpin(String pin) {
         return this.pin.equals(pin);
     }
 
-    public void deposit(double amount)
-    {
-        if(amount > 0)
-        {
+    public void deposit(double amount) {
+        if (amount > 0) {
             balance += amount;
             System.out.println("Depsited: " + amount);
-        }else{System.out.println("Invalid amount");}
+        } else {
+            System.out.println("Invalid amount");
+        }
     }
 
-    public void withdraw(double amount)
-    {
-        if(amount > 0 && amount <= balance)
-        {
+    public void withdraw(double amount) {
+        if (amount > 0 && amount <= balance) {
             balance -= amount;
             System.out.println("Withdraw: " + amount);
-        }else{System.out.println("Invalid or insufficientn balance.");}
+        } else {
+            System.out.println("Invalid or insufficient balance.");
+        }
     }
 }
